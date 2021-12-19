51077e38636275e49f0acde2d5e9fd09af13bbbd
Minami ASATO <e215701@ie.u-ryukyu.ac.jp>
2021-12-19 23:15:31 +0900

    ステップ１

---
+++ Report5.java
@@ -0,0 +1,9 @@
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+        int num;
+        num = str.length();
+
+        System.out.println(num);
+    }
+}
