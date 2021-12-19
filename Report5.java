77f9f41a13c868f892ac77ae9edfa967eeda2759
Minami ASATO <e215701@ie.u-ryukyu.ac.jp>
2021-12-19 23:17:27 +0900

    ステップ２

--- Report5.java
+++ Report5.java
@@ -1,9 +1,16 @@
 public class Report5 {
     public static void main(String[] args){
         String str = null;
-        int num;
+ /*         int num;
+  
         num = str.length();
-
-        System.out.println(num);
+ */
+        try {
+            System.out.println(str.length());
+            } catch (NullPointerException e){
+                System.out.println(e.getMessage());
+                System.out.println("NullPointerExceptionが発生");
+            }
     }
-}
+ }
+ 
